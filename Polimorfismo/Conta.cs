667c03d411631fbe0432e2afd303c5c791928407
Miller M <86807590+xX-Mill3r-Xx@users.noreply.github.com>
2025-12-11 09:55:20 -0300

    ✨ feat: estudo sobre interfaces

---
+++ Polimorfismo/Conta.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Polimorfismo
+{
+    public class CaixaEletronico : IConta
+    {
+        public string Usuario { get; set; }
+
+        public void Depositar(double valor)
+        {
+
+        }
+
+        public void Sacar(double valor)
+        {
+
+        }
+
+        public double VerSaldo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IConta
+    {
+        string Usuario { get; set; }
+
+        void Sacar(double valor);
+        void Depositar(double valor);
+        double VerSaldo();
+    }
+}
