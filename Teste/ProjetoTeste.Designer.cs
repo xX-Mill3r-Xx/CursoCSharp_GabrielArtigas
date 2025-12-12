740b5bcc5d19681dc849bd737cbdaa13211a708a
Miller M <86807590+xX-Mill3r-Xx@users.noreply.github.com>
2025-12-12 11:12:28 -0300

    ✨ feat: Iniciando o estudo do Windows Forms
    Aprofundando o conhecimento nos componentes gráficos das aplicações do tipo Windows Forms

---
+++ Teste/ProjetoTeste.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Teste
+{
+    partial class ProjetoTeste
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(12, 12);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(205, 88);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "exe";
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // ProjetoTeste
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(229, 112);
+            this.Controls.Add(this.btn1);
+            this.Name = "ProjetoTeste";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Teste";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+    }
+}
+
---
+++ Teste/ProjetoTeste.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Teste
+{
+    public partial class ProjetoTeste : Form
+    {
+        public ProjetoTeste()
+        {
+            InitializeComponent();
+        }
+    }
+}
