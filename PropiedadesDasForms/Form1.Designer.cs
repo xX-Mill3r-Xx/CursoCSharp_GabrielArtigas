2872bb27d8a0b885d3f978f0b138d919b9ad89f6
Miller M <86807590+xX-Mill3r-Xx@users.noreply.github.com>
2025-12-13 12:03:50 -0300

    ✨ feat: estudando sobre as propriedades dos formulários
    Aprofundando os conhecimentos a respeito das propriedades dos formulários Windows Forms

---
+++ PropiedadesDasForms/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace PropiedadesDasForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.Location = new System.Drawing.Point(33, 96);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(218, 68);
+            this.btnExecutar.TabIndex = 0;
+            this.btnExecutar.Text = "Executar";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnExecutar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Minha Aplicação";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecutar;
+    }
+}
+
---
+++ PropiedadesDasForms/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace PropiedadesDasForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DefineNomeDoFormulario("Propriedades das Forms");
+            DefineTamanhoForm(300, 300);
+            DefineControlBox();
+        }
+
+        private string DefineNomeDoFormulario(string nomeForm)
+        {
+            Text = nomeForm;
+            return Text;
+        }
+
+        private void DefineTamanhoForm(int x, int y)
+        {
+            Size tamanho = new Size();
+            tamanho.Width = x;
+            tamanho.Height = y;
+
+            Size = tamanho;
+        }
+
+        private void DefineControlBox(bool controle = true)
+        {
+            ControlBox = controle;
+        }
+    }
+}
