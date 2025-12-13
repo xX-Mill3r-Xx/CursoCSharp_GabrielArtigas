912a48b32ee39c439a8a0236a196be34d220c685
Miller M <86807590+xX-Mill3r-Xx@users.noreply.github.com>
2025-12-13 14:42:42 -0300

    ✨ feat: estudando sobre eventos

--- PropiedadesDasForms/Form1.Designer.cs
+++ PropiedadesDasForms/Form1.Designer.cs
@@ -30,28 +30,66 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btnExecutar = new System.Windows.Forms.Button();
+            this.lbMessage = new System.Windows.Forms.Label();
+            this.btnLimpar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnExecutar
             // 
-            this.btnExecutar.Location = new System.Drawing.Point(33, 96);
+            this.btnExecutar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExecutar.FlatAppearance.BorderSize = 0;
+            this.btnExecutar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExecutar.Location = new System.Drawing.Point(16, 416);
             this.btnExecutar.Name = "btnExecutar";
             this.btnExecutar.Size = new System.Drawing.Size(218, 68);
             this.btnExecutar.TabIndex = 0;
             this.btnExecutar.Text = "Executar";
             this.btnExecutar.UseVisualStyleBackColor = true;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            this.btnExecutar.MouseEnter += new System.EventHandler(this.btnExecutar_MouseEnter);
+            this.btnExecutar.MouseLeave += new System.EventHandler(this.btnExecutar_MouseLeave);
+            // 
+            // lbMessage
+            // 
+            this.lbMessage.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMessage.Location = new System.Drawing.Point(12, 9);
+            this.lbMessage.Name = "lbMessage";
+            this.lbMessage.Size = new System.Drawing.Size(766, 42);
+            this.lbMessage.TabIndex = 1;
+            this.lbMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLimpar.FlatAppearance.BorderSize = 0;
+            this.btnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpar.Location = new System.Drawing.Point(240, 416);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(218, 68);
+            this.btnLimpar.TabIndex = 2;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            this.btnLimpar.MouseEnter += new System.EventHandler(this.btnLimpar_MouseEnter);
+            this.btnLimpar.MouseLeave += new System.EventHandler(this.btnLimpar_MouseLeave);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(790, 496);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.lbMessage);
             this.Controls.Add(this.btnExecutar);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(806, 535);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Minha Aplicação";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
             this.ResumeLayout(false);
 
         }
@@ -59,6 +97,8 @@
         #endregion
 
         private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.Label lbMessage;
+        private System.Windows.Forms.Button btnLimpar;
     }
 }
 
--- PropiedadesDasForms/Form1.cs
+++ PropiedadesDasForms/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace PropiedadesDasForms
@@ -12,6 +13,8 @@ namespace PropiedadesDasForms
             DefineTamanhoForm(300, 300);
             DefineControlBox();
         }
+
+        #region Metodos de definição das propiedades do Formulario
 
         private string DefineNomeDoFormulario(string nomeForm)
         {
@@ -32,5 +35,92 @@ namespace PropiedadesDasForms
         {
             ControlBox = controle;
         }
+
+        private void DefineCorDeFundo()
+        {
+            this.BackColor = Color.LightGray;
+        }
+
+        private void MeuMetodo(object sender, EventArgs e)
+        {
+            MessageBox.Show("Meu método foi chamado");
+        }
+
+        private void DefineCorBotaoAoEntrar(Button button)
+        {
+            button.BackColor = Color.OrangeRed;
+            button.ForeColor = Color.White;
+            lbMessage.Text = $"Você está sobre o botão {button.Text}";
+        }
+
+        private void DefineCorDoBotaoAoSair(Button button)
+        {
+            button.BackColor = SystemColors.Control;
+            button.ForeColor = Color.Black;
+            lbMessage.Text = $"Você saiu do botão {button.Text}";
+        }
+
+        #endregion
+
+        #region Estudos sobre os eventos de um formulario
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            DefineCorDeFundo();
+        }
+
+        private void Form1_Click(object sender, System.EventArgs e)
+        {
+            MessageBox.Show("Você clicou no formulário!");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Tem certeza que deseja fechar o formulario?",
+                                                    "Atenção",
+                                                    MessageBoxButtons.YesNo,
+                                                    MessageBoxIcon.Question);
+
+            if (result == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        #endregion
+
+        #region Propiedades dos controles
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            lbMessage.Text = "Clicando no botão executar";
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            lbMessage.Text = string.Empty;
+        }
+
+        private void btnExecutar_MouseEnter(object sender, EventArgs e)
+        {
+            DefineCorBotaoAoEntrar(btnExecutar);
+        }
+
+        private void btnExecutar_MouseLeave(object sender, EventArgs e)
+        {
+            DefineCorDoBotaoAoSair(btnExecutar);
+        }
+
+        private void btnLimpar_MouseEnter(object sender, EventArgs e)
+        {
+            DefineCorBotaoAoEntrar(btnLimpar);
+        }
+
+        private void btnLimpar_MouseLeave(object sender, EventArgs e)
+        {
+            DefineCorDoBotaoAoSair(btnLimpar);
+        }
+
+        #endregion
     }
 }
