4a753e79a7350e04729622f3a9ebb1595ddffcaa
Miller M <86807590+xX-Mill3r-Xx@users.noreply.github.com>
2025-12-13 23:08:49 -0300

    ✨ feat: estudos sobre manipulação de strings

---
+++ ManipulacaoStrings/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ManipulacaoStrings
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbTeste = new System.Windows.Forms.Label();
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbTeste
+            // 
+            this.lbTeste.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTeste.Location = new System.Drawing.Point(12, 9);
+            this.lbTeste.Name = "lbTeste";
+            this.lbTeste.Size = new System.Drawing.Size(776, 187);
+            this.lbTeste.TabIndex = 0;
+            this.lbTeste.Text = "---";
+            this.lbTeste.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.Location = new System.Drawing.Point(283, 199);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(234, 53);
+            this.btnExecutar.TabIndex = 1;
+            this.btnExecutar.Text = "Executar";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExecutar);
+            this.Controls.Add(this.lbTeste);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbTeste;
+        private System.Windows.Forms.Button btnExecutar;
+    }
+}
+
---
+++ ManipulacaoStrings/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ManipulacaoStrings
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            string texto = "Este tipo de variável é alfanumérica";
+            string nome = "Miller Martins";
+            int numero = 5400;
+
+            #region Métodos para teste
+
+            //VerificacaoContanis(texto);
+            //ConverterParaString(numero);
+            //CaixaAlta(texto);
+            //CaixaBaixa(texto);
+            //IndexOf(nome, "Martins");
+            //LastIndesxOf(nome, "a");
+
+            #endregion
+        }
+
+        private bool VerificacaoContanis(string texto)
+        {
+            if (texto.Contains("tipo"))
+            {
+                lbTeste.Text = "Contem";
+                return true;
+            }
+
+            lbTeste.Text = "Não Contem";
+            return false;
+        }
+
+        private void ConverterParaString(int numero)
+        {
+            lbTeste.Text = numero.ToString();
+        }
+
+        private void CaixaAlta(string texto)
+        {
+            lbTeste.Text = texto.ToUpper();
+        }
+
+        private void CaixaBaixa(string texto)
+        {
+            lbTeste.Text = texto.ToLower();
+        }
+
+        private void IndexOf(string texto, string posicao)
+        {
+            lbTeste.Text = texto.IndexOf(posicao).ToString();
+        }
+
+        private void LastIndesxOf(string texto, string posicao)
+        {
+            lbTeste.Text = texto.LastIndexOf(posicao).ToString();
+        }
+    }
+}
