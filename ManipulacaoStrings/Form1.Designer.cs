70cecb786c0585862e3363af26211f5dd580adf5
Miller M <86807590+xX-Mill3r-Xx@users.noreply.github.com>
2025-12-14 11:54:54 -0300

    ✨ feat: continuação - Manipulação de strings

--- ManipulacaoStrings/Form1.Designer.cs
+++ ManipulacaoStrings/Form1.Designer.cs
@@ -34,19 +34,22 @@
             // 
             // lbTeste
             // 
-            this.lbTeste.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbTeste.Location = new System.Drawing.Point(12, 9);
+            this.lbTeste.Font = new System.Drawing.Font("Segoe UI Semibold", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTeste.Location = new System.Drawing.Point(14, 10);
+            this.lbTeste.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbTeste.Name = "lbTeste";
-            this.lbTeste.Size = new System.Drawing.Size(776, 187);
+            this.lbTeste.Size = new System.Drawing.Size(610, 216);
             this.lbTeste.TabIndex = 0;
             this.lbTeste.Text = "---";
             this.lbTeste.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // btnExecutar
             // 
-            this.btnExecutar.Location = new System.Drawing.Point(283, 199);
+            this.btnExecutar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExecutar.Location = new System.Drawing.Point(351, 229);
+            this.btnExecutar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btnExecutar.Name = "btnExecutar";
-            this.btnExecutar.Size = new System.Drawing.Size(234, 53);
+            this.btnExecutar.Size = new System.Drawing.Size(273, 61);
             this.btnExecutar.TabIndex = 1;
             this.btnExecutar.Text = "Executar";
             this.btnExecutar.UseVisualStyleBackColor = true;
@@ -54,13 +57,17 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(637, 301);
             this.Controls.Add(this.btnExecutar);
             this.Controls.Add(this.lbTeste);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Manipulação de Strings";
             this.ResumeLayout(false);
 
         }
--- ManipulacaoStrings/Form1.cs
+++ ManipulacaoStrings/Form1.cs
@@ -12,8 +12,8 @@ namespace ManipulacaoStrings
 
         private void btnExecutar_Click(object sender, EventArgs e)
         {
-            string texto = "Este tipo de variável é alfanumérica";
-            string nome = "Miller Martins";
+            string texto = " Este tipo de variável é alfanumérica";
+            string nome = "Martins";
             int numero = 5400;
 
             #region Métodos para teste
@@ -24,9 +24,16 @@ namespace ManipulacaoStrings
             //CaixaBaixa(texto);
             //IndexOf(nome, "Martins");
             //LastIndesxOf(nome, "a");
+            //Insert(nome);
+            //Replace(nome);
+            //Tamanho(nome);
+            //SubString(nome);
+            //ComandoSplit(texto);
 
             #endregion
         }
+
+        #region Métodos
 
         private bool VerificacaoContanis(string texto)
         {
@@ -64,5 +71,41 @@ namespace ManipulacaoStrings
         {
             lbTeste.Text = texto.LastIndexOf(posicao).ToString();
         }
+
+        private void Insert(string nome)
+        {
+            string nomeFinal = nome.Insert(0, "Miller ");
+            lbTeste.Text = nomeFinal;
+        }
+
+        private void Replace(string nome)
+        {
+            string nomeFinal = nome.Replace("M", "W");
+            lbTeste.Text = nomeFinal;
+        }
+
+        private void Tamanho(string nome)
+        {
+            lbTeste.Text = $"O nome: {nome} contem {nome.Length.ToString()} letras";
+        }
+
+        private void SubString(string nome)
+        {
+            string nomeFinal = nome.Substring(2, 3);
+            lbTeste.Text = nomeFinal;
+        }
+
+        private void ComandoSplit(string texto)
+        {
+            lbTeste.Text = string.Empty;
+            char[] separador = new char[] { ' ' };
+            string[] resultados = texto.Split(separador);
+            foreach (var item in resultados)
+            {
+                lbTeste.Text += item + "\n";
+            }
+        }
+
+        #endregion
     }
 }
