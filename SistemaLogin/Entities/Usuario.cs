41ffaaff19044f4747deea5638bc653b9e6c9a30
Miller M <86807590+xX-Mill3r-Xx@users.noreply.github.com>
2025-12-15 09:08:25 -0300

    ✨ feat: parte 2 exercício sistema login

---
+++ SistemaLogin/Entities/Usuario.cs
@@ -0,0 +1,8 @@
+﻿namespace SistemaLogin.Entities
+{
+    public class Usuario
+    {
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+    }
+}
---
+++ SistemaLogin/Seg/CadastroUsuarios.cs
@@ -0,0 +1,35 @@
+﻿using SistemaLogin.Entities;
+
+namespace SistemaLogin.Seg
+{
+    public static class CadastroUsuarios
+    {
+        private static Usuario[] _usuarios =
+        {
+            new Usuario(){ Nome = "miller", Senha = "abc123"},
+            new Usuario(){ Nome = "admin", Senha = "admin" }
+        };
+
+        private static Usuario _usuarioLogado = null;
+
+        public static Usuario UsuarioLogado 
+        { 
+            get { return _usuarioLogado; } 
+            private set { _usuarioLogado = value; }
+        }
+
+        public static bool ValidarUsuario(string nome, string senha)
+        {
+            foreach (var usuario in _usuarios)
+            {
+                if (usuario.Nome == nome && usuario.Senha == senha)
+                {
+                    UsuarioLogado = usuario;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
