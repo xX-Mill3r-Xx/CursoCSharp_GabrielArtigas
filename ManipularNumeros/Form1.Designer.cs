a7b046af87b6b2f344490248891b399ccccd4a77
Miller M <86807590+xX-Mill3r-Xx@users.noreply.github.com>
2025-12-15 14:10:01 -0300

    ✨ feat: estudando manipulação de numeros

---
+++ ManipularNumeros/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace ManipularNumeros
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.lbResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExecutar.Location = new System.Drawing.Point(156, 207);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(158, 46);
+            this.btnExecutar.TabIndex = 0;
+            this.btnExecutar.Text = "Executar";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // lbResultado
+            // 
+            this.lbResultado.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbResultado.Location = new System.Drawing.Point(12, 9);
+            this.lbResultado.Name = "lbResultado";
+            this.lbResultado.Size = new System.Drawing.Size(446, 195);
+            this.lbResultado.TabIndex = 1;
+            this.lbResultado.Text = "Resultado";
+            this.lbResultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 271);
+            this.Controls.Add(this.lbResultado);
+            this.Controls.Add(this.btnExecutar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FrmPrincipal";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Manipular Numeros";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.Label lbResultado;
+    }
+}
+
---
+++ ManipularNumeros/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ManipularNumeros
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            //MetodoTryParse();
+            //MetodoToString();
+            //ConverterHexaDecimal();
+            //ConverterFormatoMonetarios();
+            ConverterToString();
+        }
+
+        private void MetodoTryParse()
+        {
+            int numero;
+            string numeroString = "1985";
+            string textoInvalido = "abcd";
+
+            if (int.TryParse(numeroString + textoInvalido, out numero))
+                lbResultado.Text = numero.ToString();
+            else
+                lbResultado.Text = "Conversão inválida!";
+
+            // se retirar o textoInvalido, a conversão será bem-sucedida
+        }
+
+        private void MetodoToString()
+        {
+            double numero = 2024.5;
+            lbResultado.Text = numero.ToString("0000.00");
+        }
+
+        private void ConverterHexaDecimal()
+        {
+            int num = 10;
+            lbResultado.Text = num.ToString("X");
+        }
+
+        private void ConverterFormatoMonetarios()
+        {
+            decimal valor = 10.00M;
+            lbResultado.Text = valor.ToString("C");
+        }
+
+        private void ConverterToString()
+        {
+            int num = 10;
+            lbResultado.Text = Convert.ToString(num, 16);
+        }
+    }
+}
