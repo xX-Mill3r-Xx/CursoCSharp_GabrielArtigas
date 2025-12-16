928054edd1a7a54f7faeff730713ea2a262ab21b
Miller M <86807590+xX-Mill3r-Xx@users.noreply.github.com>
2025-12-16 09:22:00 -0300

    ✨ feat: iniciando o estudo sobre coleções avançadas

---
+++ Colecoes/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Colecoes
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lista = new System.Windows.Forms.ListBox();
+            this.btnList = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lista
+            // 
+            this.lista.FormattingEnabled = true;
+            this.lista.ItemHeight = 15;
+            this.lista.Location = new System.Drawing.Point(12, 12);
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(458, 499);
+            this.lista.TabIndex = 0;
+            // 
+            // btnList
+            // 
+            this.btnList.BackColor = System.Drawing.Color.Navy;
+            this.btnList.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnList.FlatAppearance.BorderSize = 0;
+            this.btnList.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Blue;
+            this.btnList.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btnList.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnList.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnList.ForeColor = System.Drawing.Color.White;
+            this.btnList.Location = new System.Drawing.Point(476, 12);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(151, 23);
+            this.btnList.TabIndex = 1;
+            this.btnList.Text = "List<>";
+            this.btnList.UseVisualStyleBackColor = false;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(639, 519);
+            this.Controls.Add(this.btnList);
+            this.Controls.Add(this.lista);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "FrmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Coleções Avançadas";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lista;
+        private System.Windows.Forms.Button btnList;
+    }
+}
+
---
+++ Colecoes/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Colecoes
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            #region exemplo de um array
+
+            //Array - Tipo Primitivo
+            string[] nomes = new string[] { "Miller", "Mateus", "Juninho" };
+
+            #endregion
+
+            #region para criar e iniciar uma lista
+
+            // podemos tambem criar uma lista vazia, sem iniciar.
+
+            List<string> nomesLista = new List<string>
+            {
+                "nome 1",
+                "nome 2",
+                "nome 3",
+                "nome 4",
+                "etc..."
+            };
+
+            #endregion
+
+            #region para adicionar nomes a lista
+
+            nomesLista.Add("Lucas");
+            nomesLista.Add("nome 5");
+
+            #endregion
+
+            #region para exibir os nomes na lista
+
+            // podemos exibor apenas os nomes selecionados com o metodo .Add()
+
+            //lista.Items.Add(nomesLista[0]);
+            //lista.Items.Add(nomesLista[1]);
+            //lista.Items.Add(nomesLista[2]);
+            //lista.Items.Add(nomesLista[3]);
+            //lista.Items.Add(nomesLista[4]);
+            //lista.Items.Add(nomesLista[5]);
+            //lista.Items.Add(nomesLista[6]);
+
+            #endregion
+
+            #region adicionando elementos usando o AddRange()
+
+            //lista.Items.AddRange(nomesLista.ToArray());
+
+            #endregion
+
+            #region usando um foreach para percorrer a lista e add os elementos
+
+            foreach (string nome in nomesLista)
+            {
+                lista.Items.Add(nome);
+            }
+
+            #endregion
+        }
+    }
+}
