4240304ecb7318a533d37ad9fda0be61e143683f
Miller M <86807590+xX-Mill3r-Xx@users.noreply.github.com>
2025-12-20 14:32:38 -0300

    ✨ feat: estudando sobre a classe message box

---
+++ ClassesImportantes/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ClassesImportantes
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMessageBox = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnMessageBox
+            // 
+            this.btnMessageBox.BackColor = System.Drawing.Color.Navy;
+            this.btnMessageBox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMessageBox.FlatAppearance.BorderSize = 0;
+            this.btnMessageBox.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Blue;
+            this.btnMessageBox.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btnMessageBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMessageBox.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMessageBox.ForeColor = System.Drawing.Color.White;
+            this.btnMessageBox.Location = new System.Drawing.Point(12, 12);
+            this.btnMessageBox.Name = "btnMessageBox";
+            this.btnMessageBox.Size = new System.Drawing.Size(149, 41);
+            this.btnMessageBox.TabIndex = 0;
+            this.btnMessageBox.Text = "MessageBox";
+            this.btnMessageBox.UseVisualStyleBackColor = false;
+            this.btnMessageBox.Click += new System.EventHandler(this.btnMessageBox_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(167, 12);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(437, 41);
+            this.lblResultado.TabIndex = 1;
+            this.lblResultado.Text = "Resultado:";
+            this.lblResultado.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 519);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnMessageBox);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "FrmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Classes Importantes";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMessageBox;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ ClassesImportantes/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ClassesImportantes
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMessageBox_Click(object sender, EventArgs e)
+        {
+            #region Texto simples
+
+            //MessageBox.Show("Teste de caixa de texto!"); // faz parte do using System.Windows.Forms
+
+            #endregion
+
+            #region Texto + Titulo
+
+            //MessageBox.Show("Mensagem", "Titulo"); // Exibe a msg e o titulo da msg
+
+            #endregion
+
+            #region Fechar Janela
+
+            //if (MessageBox.Show("Confirma fechamento da form?",
+            //    "Fechar Janela?",
+            //    MessageBoxButtons.YesNo,
+            //    MessageBoxIcon.Question) == DialogResult.Yes)
+            //    lblResultado.Text = $"Clicou no Yes - Aqui a form seria fechada.";
+            //else
+            //    lblResultado.Text = $"Clicou no No - Aqui, nada acontece";
+
+            #endregion
+
+            #region Mensagem de erro
+
+            //MessageBox.Show("Mensagem de Erro", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            #endregion
+
+            #region Seleção de botão por padrão
+
+            MessageBox.Show("Botão selecionado por padrão", "Padrão", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Information, MessageBoxDefaultButton.Button3);
+
+            #endregion
+        }
+    }
+}
