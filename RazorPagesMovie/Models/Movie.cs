72dbd1c14a30930315f2d72924dfed611e7ff3f5
Miller M <86807590+xX-Mill3r-Xx@users.noreply.github.com>
2025-12-26 09:42:38 -0300

    ✨ feat: criando a models para o app de filmes

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Genere { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public decimal Price { get; set; }
+    }
+}
