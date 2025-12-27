594d5ea4986a65bd1d6e0d5bf8b97b59b77cb2e6
Miller M <86807590+xX-Mill3r-Xx@users.noreply.github.com>
2025-12-27 11:03:48 -0300

    ✨ feat: editando o layout das paginas razor

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -5,11 +5,15 @@ namespace RazorPagesMovie.Models
     public class Movie
     {
         public int Id { get; set; }
-        public string? Title { get; set; }
-        public string? Genere { get; set; }
 
+        [Display(Name = "Titulo")]
+        public string? Title { get; set; }
+        [Display(Name = "Gênero")]
+        public string? Genere { get; set; }
+        [Display(Name = "Data de lançamento")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+        [Display(Name = "Preço")]
         public decimal Price { get; set; }
     }
 }
