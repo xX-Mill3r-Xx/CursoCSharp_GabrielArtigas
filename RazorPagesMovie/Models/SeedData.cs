2a79f2379eb900d5151635fd5019e4b188eb0d51
Miller M <86807590+xX-Mill3r-Xx@users.noreply.github.com>
2025-12-28 11:18:16 -0300

    ✨ feat: propagando o banco de dados

---
+++ RazorPagesMovie/Models/SeedData.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Data;
+
+namespace RazorPagesMovie.Models
+{
+    public class SeedData
+    {
+        public static void Initialized(IServiceProvider provider)
+        {
+            using var context = new RazorPagesMovieContext(provider.GetRequiredService<DbContextOptions<RazorPagesMovieContext>>());
+
+            if (context == null || context.Movie == null)
+                throw new ArgumentNullException($"Null: RazorPagesMovieContext");
+
+            if (context.Movie.Any())
+                return;
+
+            context.Movie.AddRange(
+                new Movie
+                {
+                    Title = "O Esterminador do Futuro",
+                    Genere = "Ação - Ficção Cientifica",
+                    ReleaseDate = DateTime.Parse("1987-2-12"),
+                    Price = 15.50M
+                },
+
+                new Movie
+                {
+                    Title = "O Esterminador do Futuro 2",
+                    Genere = "Ação - Ficção Cientifica",
+                    ReleaseDate = DateTime.Parse("1992-4-6"),
+                    Price = 9.00M
+                },
+
+                new Movie
+                {
+                    Title = "Ghostbusters",
+                    Genere = "Comédia",
+                    ReleaseDate = DateTime.Parse("1984-3-10"),
+                    Price = 8.99M
+                }
+            );
+
+            context.SaveChanges();
+        }
+    }
+}
