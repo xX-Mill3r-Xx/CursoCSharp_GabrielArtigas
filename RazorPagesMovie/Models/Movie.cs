ffa5223279693fdc0f57ebfc4693c7793e06ff38
Miller M <86807590+xX-Mill3r-Xx@users.noreply.github.com>
2025-12-28 16:02:28 -0300

    ✨ feat: atualizando o modelo

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models
 {
@@ -13,6 +14,7 @@ namespace RazorPagesMovie.Models
         [Display(Name = "Data de lançamento")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
         [Display(Name = "Preço")]
         public decimal Price { get; set; }
     }
