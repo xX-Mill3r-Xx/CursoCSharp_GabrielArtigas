ef195fe252bab587afd528cb9205b173662e21a3
Miller M <86807590+xX-Mill3r-Xx@users.noreply.github.com>
2026-01-01 12:37:47 -0300

    ✨ feat: estudando sobre estruturas de controle

---
+++ EstruturasDeControle/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace EstruturasDeControle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Estrutura de controle - switch case
+            //int escolha = 1;
+
+            //switch (escolha)
+            //{
+            //    default:
+            //        Console.WriteLine("opção default");
+            //        break;
+            //    case 1:
+            //        Console.WriteLine("opção 1");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("opção 2");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("opção 3");
+            //        break;
+            //}
+            #endregion
+
+            #region Declaração Goto
+
+            //Inicio:
+
+            //Console.Write("escolha uma opção: ");
+            //int op = int.Parse(Console.ReadLine());
+            //int valor = 0;
+
+            //switch (op)
+            //{
+            //    default:
+            //        goto Inicio;
+            //    case 1:
+            //        valor += 100;
+            //        break;
+            //    case 2:
+            //        valor += 50;
+            //        goto case 1;
+
+            //}
+
+            #endregion
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
