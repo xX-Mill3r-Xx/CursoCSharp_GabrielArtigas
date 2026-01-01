847b041699650092f10d50b2b01632eee89f8b0e
Miller M <86807590+xX-Mill3r-Xx@users.noreply.github.com>
2026-01-01 12:44:27 -0300

    ✨ feat: add exercício sequencia fibonacci

---
+++ SequenciaFibonaci/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SequenciaFibonaci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 1;
+            int c = 0;
+
+            Console.Write("Quantos Valores: ");
+            int valores = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Sequencia de Fibonacci com " + valores + "Valores");
+
+            for (int i = 0; i < valores; i++)
+            {
+                if (i < valores - 1)
+                {
+                    Console.Write(a + ", ");
+                }
+                else
+                {
+                    Console.Write(a);
+                }
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            Console.ReadKey();
+        }
+    }
+}
