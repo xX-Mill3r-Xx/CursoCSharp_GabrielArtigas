4c493d6ca054c898e3754d8a01d8134774ff6c4f
Miller M <86807590+xX-Mill3r-Xx@users.noreply.github.com>
2026-01-01 12:42:38 -0300

    ✨ feat: estudando sobre ciclos de repetição

---
+++ EstruturasDeRepeticao/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace EstruturasDeRepeticao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Estrutura while
+            //int valor = 0;
+
+            //while (valor < 10)
+            //{
+            //    Console.WriteLine("O valor é: " + valor);
+            //    valor++;
+            //}
+            //Console.ReadLine();
+            #endregion
+
+            #region ciclo do-while
+
+            //int valor = 15;
+
+            //do
+            //{
+            //    Console.WriteLine("o valor é: " + valor);
+            //    valor++;
+            //} while (valor < 10);
+            //Console.ReadLine();
+
+            #endregion
+
+            #region ciclo for
+
+            //for (int i = 0, j = 10; i < 10; i++, j--)
+            //{
+            //    Console.WriteLine("I = " + i + "| j = " + j);
+            //}
+
+            //Console.ReadKey();
+            #endregion
+
+            #region ciclo Forech
+
+            //string[] nomes = { "Miller", "Martins", "Vasco" };
+
+            //foreach (string nome in nomes)
+            //{
+            //    Console.WriteLine("Nome: " + nome);
+            //}
+
+            //Console.ReadKey();
+            #endregion
+        }
+    }
+}
