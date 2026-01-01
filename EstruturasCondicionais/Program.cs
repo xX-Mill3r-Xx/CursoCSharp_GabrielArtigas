f0397e2a41f6bf3ad5bb5f8e5af259f315db220b
Miller M <86807590+xX-Mill3r-Xx@users.noreply.github.com>
2026-01-01 12:28:49 -0300

    ✨ feat: estudando sobre estruturas condicionais

---
+++ EstruturasCondicionais/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EstruturasCondicionais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Tomando decisões com o comando if-else");
+
+            // Operadores boleanos;
+            // ==, !=, >, <, >=, <=,
+
+            Console.Write("Digite o primeiro valor: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo valor: ");
+            int b = int.Parse(Console.ReadLine());
+
+            int res = 500;
+
+            if ((a + b) > res)
+            {
+                Console.WriteLine("Verdadeiro");
+            }
+            else
+            {
+                Console.WriteLine("Falso");
+            }
+        }
+    }
+}
