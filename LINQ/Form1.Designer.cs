8c8c3065cc7e790276c46f9745d9c3072efd9a2d
Miller M <86807590+xX-Mill3r-Xx@users.noreply.github.com>
2026-01-02 10:46:51 -0300

    ✨ feat: iniciando o estudo do LINQ

---
+++ LINQ/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace LINQ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lista = new System.Windows.Forms.ListBox();
+            this.txtConsulta = new System.Windows.Forms.TextBox();
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lista
+            // 
+            this.lista.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lista.FormattingEnabled = true;
+            this.lista.ItemHeight = 32;
+            this.lista.Location = new System.Drawing.Point(12, 12);
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(490, 548);
+            this.lista.TabIndex = 0;
+            // 
+            // txtConsulta
+            // 
+            this.txtConsulta.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtConsulta.Location = new System.Drawing.Point(508, 12);
+            this.txtConsulta.Name = "txtConsulta";
+            this.txtConsulta.Size = new System.Drawing.Size(321, 23);
+            this.txtConsulta.TabIndex = 1;
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.BackColor = System.Drawing.Color.Navy;
+            this.btnExecutar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExecutar.FlatAppearance.BorderSize = 0;
+            this.btnExecutar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Blue;
+            this.btnExecutar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btnExecutar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExecutar.ForeColor = System.Drawing.Color.White;
+            this.btnExecutar.Location = new System.Drawing.Point(508, 41);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(321, 44);
+            this.btnExecutar.TabIndex = 2;
+            this.btnExecutar.Text = "Executar";
+            this.btnExecutar.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(841, 596);
+            this.Controls.Add(this.btnExecutar);
+            this.Controls.Add(this.txtConsulta);
+            this.Controls.Add(this.lista);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LINQ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lista;
+        private System.Windows.Forms.TextBox txtConsulta;
+        private System.Windows.Forms.Button btnExecutar;
+    }
+}
+
